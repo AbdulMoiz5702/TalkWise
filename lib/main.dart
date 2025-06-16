da5d218e10a351fb95a8b9f4a17537eeddf34f58
AbdulMoiz5702 <abdulmoizkoikenx10@gmail.com>
2025-06-16 11:32:52 +0500

    main.dart_updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,37 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'Bloc/chat_bot_bloc.dart';
 import 'Views/Screens/gemini_page.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final results = await Future.wait([
+        () async {
+      if (kIsWeb) {
+        return await HydratedStorage.build(
+          storageDirectory: HydratedStorageDirectory.web,
+        );
+      } else {
+        final dir = await getTemporaryDirectory();
+        return await HydratedStorage.build(
+          storageDirectory: HydratedStorageDirectory((dir).path),
+        );
+      }
+    }(),
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]),
+  ]);
+  HydratedBloc.storage = results[0] as HydratedStorage;
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
